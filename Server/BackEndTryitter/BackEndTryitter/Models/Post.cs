64326a3353c16b5daa3e94ca49d59efb55a5e828
André Morsch <andremorsch37@gmail.com>
2023-02-09 15:57:55 -0300

    feat: create model Post

---
+++ Server/BackEndTryitter/BackEndTryitter/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace BackEndTryitter.Models
+{
+    public class Post
+    {
+        [Key]
+        public Guid PostId { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
