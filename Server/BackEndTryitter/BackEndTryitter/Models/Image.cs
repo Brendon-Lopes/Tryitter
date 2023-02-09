8a2c1f020ad2ea90576e0ab4e1c6fa00bb3b2007
André Morsch <andremorsch37@gmail.com>
2023-02-09 16:01:58 -0300

    feat: create model Image

---
+++ Server/BackEndTryitter/BackEndTryitter/Models/Image.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackEndTryitter.Models
+{
+    public class Image
+    {
+        [Key]
+        public Guid ImageId { get; set; }
+        public string ImageUrl { get; set; }
+        
+        [ForeignKey("PostId")]
+        public Post Post { get; set; }
+        public Guid PostId { get; set; }
+    }
+}
