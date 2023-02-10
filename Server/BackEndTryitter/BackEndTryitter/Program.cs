f2704156ab1fa1bf66869ffc7480207dc7afe672
Brendon <brendon.lopes.21@gmail.com>
2023-02-10 00:58:44 -0300

    feat: update builder services
    adding dependencies injections where needed.

--- Server/BackEndTryitter/BackEndTryitter/Program.cs
+++ Server/BackEndTryitter/BackEndTryitter/Program.cs
@@ -1,11 +1,19 @@
 using BackEndTryitter.Contexts;
+using BackEndTryitter.Repositories;
+using BackEndTryitter.Services.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TryitterContext>();
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<ITryitterContext, TryitterContext>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
