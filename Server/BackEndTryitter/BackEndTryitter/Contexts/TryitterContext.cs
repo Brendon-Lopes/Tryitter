599570dc2f3d5481879574758f1d6b66637c8dec
Brendon <brendon.lopes.21@gmail.com>
2023-02-10 00:58:14 -0300

    refactor: TryitterContext implement ITryitterContext interface
    also properties receive a non-nullable value as default.

--- Server/BackEndTryitter/BackEndTryitter/Contexts/TryitterContext.cs
+++ Server/BackEndTryitter/BackEndTryitter/Contexts/TryitterContext.cs
@@ -3,15 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BackEndTryitter.Contexts;
 
-public class TryitterContext : DbContext
+public class TryitterContext : DbContext, ITryitterContext
 {
     public TryitterContext(DbContextOptions<TryitterContext> options)
         : base(options)
     { }
 
-    public DbSet<User> Users { get; set; }
-    public DbSet<Post> Posts { get; set; }
-    public DbSet<Image> Images { get; set; }
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Post> Posts { get; set; } = null!;
+    public DbSet<Image> Images { get; set; } = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
