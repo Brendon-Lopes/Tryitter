9b59f377415082a2e0aab3972d5d299d6d48f257
Brendon <brendon.lopes.21@gmail.com>
2023-02-11 23:37:55 -0300

    chore: add error handling middleware
    to the app builder

--- Server/BackEndTryitter/BackEndTryitter/Program.cs
+++ Server/BackEndTryitter/BackEndTryitter/Program.cs
@@ -1,4 +1,5 @@
 using BackEndTryitter.Contexts;
+using BackEndTryitter.Middleware;
 using BackEndTryitter.Repositories;
 using BackEndTryitter.Services.Authentication;
 
@@ -26,6 +27,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
