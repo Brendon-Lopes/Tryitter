247452ee8216c5bb81dc608a5d4c2d8bea59bd8b
Brendon <brendon.lopes.21@gmail.com>
2023-02-13 03:16:01 -0300

    refactor: move helper method 'CheckAuthorization'
    to a separate static class 'AuthorizationServices'

--- Server/BackEndTryitter/BackEndTryitter/Controllers/UserController.cs
+++ Server/BackEndTryitter/BackEndTryitter/Controllers/UserController.cs
@@ -1,6 +1,6 @@
-using System.Security.Claims;
 using BackEndTryitter.Contracts.User;
 using BackEndTryitter.Repositories;
+using BackEndTryitter.Services.Authorization;
 using BackEndTryitter.Services.Validators;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -23,7 +23,7 @@ public class UserController : ControllerBase
     [Route("{id}/status")]
     public IActionResult UpdateUserStatus([FromRoute] Guid id, [FromBody] UpdateUserStatusRequest request)
     {
-        if (!CheckAuthorization(HttpContext, id)) return Unauthorized();
+        if (!AuthorizationServices.CheckAuthorization(HttpContext, id)) return Unauthorized();
 
         var validator = new UpdateUserStatusValidator();
         var validationResult = validator.Validate(request);
@@ -45,19 +45,10 @@ public class UserController : ControllerBase
     [Route("{id}")]
     public IActionResult DeleteUser([FromRoute] Guid id)
     {
-        if (!CheckAuthorization(HttpContext, id)) return Unauthorized();
+        if (!AuthorizationServices.CheckAuthorization(HttpContext, id)) return Unauthorized();
 
         _userRepository.Delete(id);
 
         return NoContent();
-    }
-
-    private static bool CheckAuthorization(HttpContext httpContext, Guid id)
-    {
-        var claimsIdentity = httpContext.User.Identity as ClaimsIdentity;
-        if (claimsIdentity == null) return false;
-
-        var tokenId = claimsIdentity.Claims.First(c => c.Type == "userId").Value;
-        return tokenId == id.ToString();
     }
 }
