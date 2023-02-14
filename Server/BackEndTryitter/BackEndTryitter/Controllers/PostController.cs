e5030611b6080f1d9da8e6976f6dba66ee046ab1
André Morsch <andremorsch37@gmail.com>
2023-02-14 01:07:54 -0300

    feat: create Post Controller

---
+++ Server/BackEndTryitter/BackEndTryitter/Controllers/PostController.cs
@@ -0,0 +1,114 @@
+﻿using BackEndTryitter.Contracts.Post;
+using BackEndTryitter.Models;
+using BackEndTryitter.Repositories;
+using BackEndTryitter.Services.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEndTryitter.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("/posts")]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [AllowAnonymous]
+        public IActionResult GetPostById([FromRoute] Guid id)
+        {
+            var post = _postRepository.GetPostById(id);
+
+            if (post == null)
+                return NotFound();
+
+            return Ok(post);
+        }
+
+        [HttpGet]
+        [Route("user/{username}")]
+        [AllowAnonymous]
+        public IActionResult GetPostsByUsername([FromRoute] string username)
+        {
+            var posts = _postRepository.GetAllPostsByUsername(username);
+
+            if (!posts.Any())
+                return NotFound();
+
+            return Ok(posts);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult GetAllPosts()
+        {
+            var posts = _postRepository.GetAllPosts();
+
+            if (!posts.Any())
+                return NotFound();
+
+            return Ok(posts);
+        }
+
+        [HttpPost]
+        public IActionResult AddPost([FromBody] PostRequest post)
+        {
+            if (!AuthorizationServices.CheckAuthorization(HttpContext, post.userId))
+                return Unauthorized();
+
+            Post addPost = new()
+            {
+                PostId = Guid.NewGuid(),
+                UserId = post.userId,
+                Text = post.text,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+        
+            _postRepository.AddPost(addPost);
+
+            return CreatedAtAction(nameof(GetPostById), new { id = addPost.PostId }, addPost);
+        }
+
+        [HttpPatch]
+        [Route("{PostId}")]
+        public IActionResult UpdatePost([FromRoute] Guid postId, [FromBody] PostText text)
+        {
+            var postToUpdate = _postRepository.GetPostById(postId);
+            
+            if (postToUpdate == null)
+                return NotFound();
+            
+            if (!AuthorizationServices.CheckAuthorization(HttpContext, postToUpdate.UserId))
+                return Unauthorized();
+
+            _postRepository.UpdateTextPost(postId, text.text);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{PostId}")]
+        public IActionResult DeletePost([FromRoute] Guid postId)
+        {
+            var postToUpdate = _postRepository.GetPostById(postId);
+
+            if (postToUpdate == null)
+                return NotFound();
+
+            if (!AuthorizationServices.CheckAuthorization(HttpContext, postToUpdate.UserId))
+                return Unauthorized();
+
+            _postRepository.DeletePost(postId);
+
+            return NoContent();
+        }
+    }
+}
