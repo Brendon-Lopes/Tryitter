7f4803bdb5c77ba26631a54a0f74a21b82911694
André Morsch <andremorsch37@gmail.com>
2023-02-14 01:06:37 -0300

    feat: create Interface Post Repository

---
+++ Server/BackEndTryitter/BackEndTryitter/Repositories/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using BackEndTryitter.Models;
+
+namespace BackEndTryitter.Repositories
+{
+    public interface IPostRepository
+    {
+        Post? GetPostById(Guid Id);
+        List<Post> GetAllPostsByUsername(string username);
+        List<Post> GetAllPosts();
+        void AddPost(Post post);
+        void UpdateTextPost(Guid id, string text);
+        void DeletePost(Guid id);
+    }
+}
