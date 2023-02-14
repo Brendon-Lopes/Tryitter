fd9e93683b81514175f2a3a1f4457fa58da81971
André Morsch <andremorsch37@gmail.com>
2023-02-14 01:06:52 -0300

    feat: create Post Repository

---
+++ Server/BackEndTryitter/BackEndTryitter/Repositories/PostRepository.cs
@@ -0,0 +1,65 @@
+﻿using BackEndTryitter.Contexts;
+using BackEndTryitter.Contracts.Post;
+using BackEndTryitter.Exceptions;
+using BackEndTryitter.Models;
+using System.Net;
+
+namespace BackEndTryitter.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly TryitterContext _context;
+
+        public PostRepository(TryitterContext context)
+        {
+            _context = context;
+        }
+
+        public Post? GetPostById(Guid Id)
+        {
+            return _context.Posts.FirstOrDefault(p => p.PostId == Id);
+        }
+
+        public List<Post> GetAllPostsByUsername(string username)
+        {
+            return _context.Posts.Where(p => p.User.Username == username).ToList();
+        }
+
+        public List<Post> GetAllPosts()
+        {
+            return _context.Posts.ToList();
+        }
+
+        public void AddPost(Post post)
+        {
+            _context.Posts.Add(post);
+
+            _context.SaveChanges();
+        }
+
+        public void UpdateTextPost(Guid id, string text)
+        {
+            var post = _context.Posts.FirstOrDefault(p => p.PostId == id);
+            
+            if (post == null)
+                throw new CustomException(HttpStatusCode.NotFound, "Post not found");
+
+            post.Text = text;
+            post.UpdatedAt = DateTime.UtcNow;
+
+            _context.SaveChanges();
+        }
+
+        public void DeletePost(Guid id)
+        {
+            var post = _context.Posts.FirstOrDefault(p => p.PostId == id);
+
+            if (post == null)
+                throw new CustomException(HttpStatusCode.NotFound, "Post not found");
+
+            _context.Posts.Remove(post);
+
+            _context.SaveChanges();
+        }
+    }
+}
